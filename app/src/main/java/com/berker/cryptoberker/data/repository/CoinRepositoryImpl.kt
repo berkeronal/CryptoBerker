0071e737418b7d11adee1b07f044dbdc16d59f21
berkeronal <berker435@gmail.com>
2021-11-04 23:35:52 +0300

    Added Repository (A fake repo simulates api for future testing and replacement purposes)

---
+++ app/src/main/java/com/berker/cryptoberker/data/repository/CoinRepositoryImpl.kt
@@ -0,0 +1,19 @@
+package com.berker.cryptoberker.data.repository
+
+import com.berker.cryptoberker.data.remote.CoinpaprikaApi
+import com.berker.cryptoberker.data.remote.dto.CoinDetailDto
+import com.berker.cryptoberker.data.remote.dto.CoinDto
+import com.berker.cryptoberker.domain.repository.CoinRepository
+import javax.inject.Inject
+
+class CoinRepositoryImpl @Inject constructor(
+    private val api: CoinpaprikaApi,
+) : CoinRepository {
+    override suspend fun getCoins(): List<CoinDto> {
+        return api.getCoins()
+    }
+
+    override suspend fun getCoinById(coinId: String): CoinDetailDto {
+        return api.getCoinById(coinId)
+    }
+}
